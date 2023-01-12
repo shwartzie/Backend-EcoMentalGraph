e6274b2e96e4445bd0713519c8b044233d1aae9b
Roni <ronnybusiness77@gmail.com>
2023-01-12 20:08:40 +0200

    dev file updated

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
 	dbURL:
-		'mongodb+srv://shwartzie:410896Roni@orello.djgmygm.mongodb.net/?retryWrites=true&w=majority'
+		'mongodb+srv://oren@cluster0.ftmm9.mongodb.net/admin?replicaSet=atlas-3sm9p7-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=atlas-3sm9p7-shard-0&3t.databases=admin,feel-good&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true&3t.sslTlsVersion=TLS'
 }
